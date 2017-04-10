16afbca1fd8782348dd9a46ea61075cdb69371e8
Vitali Novik <Vitali_Novik@epam.com>
2017-04-10 11:13:18 +0300

    Module1: Created product struct with tests

---
+++ OnlineStoreProduct.Tests/ProductTest.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OnlineStoreProduct.Tests
+{
+	[TestClass]
+	public class ProductTest
+	{
+		[TestMethod]
+		public void Equals_NullObject_ReturnsFalse()
+		{
+			Product product = new Product();
+
+			bool result = product.Equals(null);
+
+			Assert.IsFalse(result);
+		}
+
+		[TestMethod]
+		public void Equals_NotProduct_ReturnsFalse()
+		{
+			Product product = new Product("product", 1m, 1);
+			var obj = new
+			{
+				Name = "product",
+				Price = 1m,
+				Amount = 1
+			};
+
+			bool result = product.Equals(obj);
+
+			Assert.IsFalse(result);
+		}
+
+		[TestMethod]
+		public void Equals_SameInstances_ReturnsTrue()
+		{
+			Product product = new Product();
+
+			bool result = product.Equals(product);
+
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void Equals_SymmetricCalls_ReturnsFalse()
+		{
+			Product productA = new Product("productA", 123m, 12);
+			Product productB = new Product("productB", 124m, 12);
+
+			bool straight = productA.Equals(productB);
+			bool reverse = productB.Equals(productA);
+
+			Assert.IsFalse(straight);
+			Assert.IsFalse(reverse);
+		}
+
+		[TestMethod]
+		public void Equals_SymmetricCalls_ReturnsTrue()
+		{
+			Product productA = new Product("productA", 123m, 12);
+			Product productB = new Product("productA", 123m, 12);
+
+			bool straight = productA.Equals(productB);
+			bool reverse = productB.Equals(productA);
+
+			Assert.IsTrue(straight);
+			Assert.IsTrue(reverse);
+		}
+
+		[TestMethod]
+		public void Equals_TransitiveCalls_ReturnsFalse()
+		{
+			Product productA = new Product("productA", 123m, 12);
+			Product productB = new Product("productA", 123.1m, 12);
+			Product productC = new Product("productC", 123m, 13);
+
+			bool result = productA.Equals(productB) && productB.Equals(productC);
+			bool transitive = productA.Equals(productC);
+
+			Assert.IsFalse(result);
+			Assert.IsFalse(transitive);
+		}
+
+		[TestMethod]
+		public void Equals_TransitiveCalls_ReturnsTrue()
+		{
+			Product productA = new Product("productA", 123m, 12);
+			Product productB = new Product("productA", 123m, 12);
+			Product productC = new Product("productA", 123m, 12);
+
+			bool result = productA.Equals(productB) && productB.Equals(productC);
+			bool transitive = productA.Equals(productC);
+
+			Assert.IsTrue(result);
+			Assert.IsTrue(transitive);
+		}
+
+		[TestMethod]
+		public void EqualsReturnsTrue_HashCodesEquals()
+		{
+			Product productA = new Product("productA", 123m, 12);
+			Product productB = new Product("productA", 123m, 12);
+
+			if (productA.Equals(productB))
+			{
+				Assert.IsTrue(productA.GetHashCode() == productB.GetHashCode());
+			}
+		}
+
+		[TestMethod]
+		public void HashCodesDifferent_EqualsReturnsFalse()
+		{
+			Product productA = new Product("productA", 123m, 12);
+			Product productB = new Product("productA", 123.1m, 12);
+
+			if (productA.GetHashCode() != productB.GetHashCode())
+			{
+				Assert.IsFalse(productA.Equals(productB));
+			}
+		}
+	}
+}
---
+++ OnlineStoreProduct/Product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace OnlineStoreProduct
+{
+	public struct Product
+	{
+		public string Name { get; }
+		public decimal Price { get; }
+		public int Amount { get; }
+		
+		public Product(string name, decimal price, int amount)
+		{
+			if (string.IsNullOrEmpty(name))
+			{
+				throw new ArgumentException(nameof(name));
+			}
+
+			if (price < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(price));
+			}
+
+			if (amount < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(amount));
+			}
+
+			this.Name = name;
+			this.Price = price;
+			this.Amount = amount;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Product))
+			{
+				return false;
+			}
+
+			return this.Equals((Product) obj);
+		}
+
+		public bool Equals(Product product)
+		{
+			return (product.Name == this.Name) && (product.Price == this.Price) && (product.Amount == this.Amount);
+		}
+
+		public override int GetHashCode()
+		{
+			return this.Name.GetHashCode() ^ this.Price.GetHashCode() ^ this.Amount.GetHashCode();
+		}
+
+		public static bool operator ==(Product productA, Product productB)
+		{
+			return productA.Equals(productB);
+		}
+
+		public static bool operator !=(Product productA, Product productB)
+		{
+			return !(productA == productB);
+		}
+	}
+}
