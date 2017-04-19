052a7ba21a25678921bed52a495c405a16555bcb
Vitaliy <vitaliy.novik.94@gmail.com>
2017-04-20 01:47:13 +0300

    DecimalRangeAttribute with tests

---
+++ Attributes.Tests/DecimalRangeAttributeTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Attributes.Tests
+{
+	[TestClass]
+	public class DecimalRangeAttributeTest
+	{
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IsValid_NullObject_ThrowsException()
+		{
+			DecimalRangeAttribute attribute = new DecimalRangeAttribute(0m, 1m);
+
+			attribute.IsValid(null);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void IsValid_NotDecimal_ThrowsException()
+		{
+			DecimalRangeAttribute attribute = new DecimalRangeAttribute(0m, 2m);
+
+			attribute.IsValid(2);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void IsValid_MaxLessThanMin_ThrowsException()
+		{
+			DecimalRangeAttribute attribute = new DecimalRangeAttribute(2.1m, 2m);
+
+			attribute.IsValid(2);
+		}
+
+		[TestMethod]
+		public void IsValid_ValueOutOfRange_ReturnsFalse()
+		{
+			DecimalRangeAttribute attribute = new DecimalRangeAttribute(2m, 2m);
+
+			attribute.IsValid(2.002m);
+		}
+
+		[TestMethod]
+		public void IsValid_ValueInRange_ReturnsTrue()
+		{
+			DecimalRangeAttribute attribute = new DecimalRangeAttribute(2m, 2.1m);
+
+			attribute.IsValid(2.15m);
+		}
+
+		[TestMethod]
+		public void IsValid_ValueOnBorder_ReturnsTrue()
+		{
+			DecimalRangeAttribute attribute = new DecimalRangeAttribute(decimal.MinValue, decimal.MaxValue);
+
+			attribute.IsValid(decimal.MaxValue);
+		}
+	}
+}
---
+++ Attributes/DecimalRangeAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Attributes
+{
+	public class DecimalRangeAttribute : ValidationAttribute
+	{
+		public decimal Min { get; }
+		public decimal Max { get; }
+
+		public DecimalRangeAttribute(decimal min, decimal max)
+		{
+			if (min > max)
+			{
+				throw new ArgumentException($"{nameof(max)} should be equal or greater than {nameof(min)}");
+			}
+
+			this.Min = min;
+			this.Max = max;
+		}
+
+		public override bool IsValid(object value)
+		{
+			if (!(value is decimal))
+			{
+				throw new InvalidOperationException();
+			}
+
+			decimal decimalValue = (decimal)value;
+
+			return decimalValue >= this.Min && decimalValue <= this.Max;
+		}
+
+	}
+}
