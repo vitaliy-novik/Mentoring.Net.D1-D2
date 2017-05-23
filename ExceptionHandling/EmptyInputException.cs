46066ebe454362cb05cc2834b8e5a5566989d040
Vitaliy <vitaliy.novik.94@gmail.com>
2017-05-23 09:47:43 +0300

    Exception Handling

---
+++ ExceptionHandling/EmptyInputException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ExceptionHandling
+{
+	[Serializable]
+	class EmptyInputException : Exception
+	{
+		private const string ErrorMessage = "Input can not be empty";
+
+		public EmptyInputException() : base(ErrorMessage) { }
+
+		public EmptyInputException(string message) : base(message) { }
+
+		public EmptyInputException(string message, Exception innerException) : base(message, innerException) { }
+
+		protected EmptyInputException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+		public override string Message => string.IsNullOrEmpty(base.Message) ? ErrorMessage : base.Message;
+	}
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("Enter any symbols:");
+				string input = Console.ReadLine();
+				try
+				{
+					string output = TruncateInput(input);
+					Console.WriteLine(output);
+				}
+				catch (EmptyInputException exception)
+				{
+					Console.WriteLine(exception.Message);
+				}
+			}
+		}
+
+		static string TruncateInput(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				throw new EmptyInputException();
+			}
+
+			return input.Substring(0, 1);
+		}
+	}
+}
