5804f6e628e54ee91288f7526f9bbcfb3dc5f8e2
Vitaliy <vitaliy.novik.94@gmail.com>
2017-06-05 09:55:27 +0300

    Reflection

---
+++ Reflection.Tests/ReflectionUtilsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Reflection.Tests
+{
+	[TestClass]
+	public class ReflectionUtilsTest
+	{
+		[TestMethod]
+		public void CreateList_Int()
+		{
+			var list = ReflectionUtils.CreateList<int>();
+
+			Assert.AreEqual(typeof(List<int>), list.GetType());
+			ReflectionUtils.AddListItem<int>(list, 4);
+		}
+	}
+}
---
+++ Reflection/ReflectionUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reflection
+{
+	public static class ReflectionUtils
+	{
+		public static List<T> CreateList<T>()
+		{
+			Type paramType = typeof(T);
+			Type listType = typeof(List<T>);
+
+			//Type constructedListType = listType.MakeGenericType(paramType);
+			List<T> instance = (List<T>)Activator.CreateInstance(listType);
+
+			return instance;
+		}
+
+		public static void AddListItem<T>(List<T> list, T item)
+		{
+			Type listType = list.GetType();
+			listType.GetMethod("Add").Invoke(list, new [] { (object)item });
+		}
+	}
+}
