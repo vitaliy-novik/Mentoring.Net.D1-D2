02b0898bab5c7c1137941271090290ce867280d0
Vitali Novik <Vitali_Novik@epam.com>
2017-06-05 19:13:27 +0300

    temp commit

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("Enter any symbols:");
+				string input = Console.ReadLine();
+				try
+				{
+					string output = TruncateInput(input);
+					Console.WriteLine(output);
+				}
+				catch (EmptyInputException exception)
+				{
+					Console.WriteLine(exception.Message);
+				}
+			}
+		}
+
+		static string TruncateInput(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				throw new EmptyInputException();
+			}
+
+			return input.Substring(0, 1);
+		}
+	}
+}
